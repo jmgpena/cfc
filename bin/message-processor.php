33d4786958d52ea3d4c51db546dcaaa1ff76aee7
jmgpena <jorge@jmgpena.net>
2015-05-26 22:22:40 +0100

    Improve connection handling
    Document unclear parts of the code

--- bin/message-processor.php
+++ bin/message-processor.php
@@ -4,17 +4,25 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use WebSocket\Client;
 use Pheanstalk\Pheanstalk;
 
+// Wait time in seconds when there are no clients connected
+define('WAIT_TIME',1);
+
 $pheanstalk = new Pheanstalk('127.0.0.1');
 
-function publishMessage($message) {
-    $client = new Client("ws://localhost:10800");
+$wsClient = new Client("ws://localhost:10800");
+
+// Publish the job/message to the websocket server
+function publishMessage($client, $message) {
     $client->send($message);
 
     $result = $client->receive();
 
+    $client->close();
     return $result == "OK";
 }
 
+// will read messages from beanstalkd queue and block
+// when no messages are available
 while (true) {
     global $pheanstalk;
 
@@ -28,13 +36,15 @@ while (true) {
     echo "Reserved job: {$job->getId()}";
     $data = $job->getData();
 
+    // if the message is succefull published to clients
+    // it will be deleted, else we wait before trying again
     if ( publishMessage($data) ) {
         $pheanstalk->delete($job);
         echo "deleted job: {$job->getId()}";
     } else {
         $pheanstalk->release($job);
         echo "released job: {$job->getId()}";
-        sleep(1);
+        sleep(WAIT_TIME);
     }
 }
 
