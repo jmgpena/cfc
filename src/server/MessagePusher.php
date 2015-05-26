52e79a74c13817c340bc70991a9405e8d3a9f79a
jmgpena <jorge@jmgpena.net>
2015-05-26 22:01:51 +0100

    Update MessagePusher class in ws-server
    Keep track of clients and workers
    Only accept messages from workers
    Send 'WAIT' message to workers when no client is connected
    Broadcast messages to all clients

--- src/server/MessagePusher.php
+++ src/server/MessagePusher.php
@@ -6,30 +6,59 @@ use Ratchet\ConnectionInterface;
 
 class MessagePusher implements MessageComponentInterface {
     protected $clients;
+    protected $workers;
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+        $this->workers = new \SplObjectStorage;
+    }
+
+    private function isWorker($conn) {
+        $ip = $conn->WebSocket->request->getHeader('X-Real-IP');
+
+        // if there is a X-Real-IP header then it's not a local worker
+        // the header is added by nginx so the client has no control over it
+        return (!$ip);
+    }
+
+    private function pushMessage($from, $msg) {
+        echo "Message from ({$from->resourceId}): ({$msg})\n";
+
+        if ( $this->clients->count() > 0 ) {
+            // broadcast message to all clients
+            $from->send('OK');
+            foreach ($this->clients as $client) {
+                $client->send($msg);
+            }
+        } else {
+            $from->send('WAITING');
+        }
     }
 
     public function onOpen(ConnectionInterface $conn) {
-        $this->clients->attach($conn);
-
-        echo "New connection! ({$conn->resourceId})\n";
+        if ( $this->isWorker($conn) ) {
+            $this->workers->attach($conn);
+            echo "New worker! ({$conn->resourceId})\n";
+        } else {
+            $this->clients->attach($conn);
+            echo "New client! ({$conn->resourceId})\n";
+        }
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        print_r($from->WebSocket->request->getHeader('X-Real-IP'));
-        //print_r($from);
-        if ( $from->remoteAddress == '127.0.0.1') {
-            echo "Message from ({$from->resourceId}): ({$msg})\n";
+        if ( $this->workers->contains($from) ) {
+            $this->pushMessage($from, $msg);
         } else {
             echo "Ignoring this message...\n";
         }
     }
 
     public function onClose(ConnectionInterface $conn) {
-        $this->clients->detach($conn);
-
+        if ( $this->clients->contains($conn) ) {
+            $this->clients->detach($conn);
+        } else {
+            $this->workers->detach($conn);
+        }
         echo "Connection ({$conn->resourceId}) has disconnected\n";
     }
 
